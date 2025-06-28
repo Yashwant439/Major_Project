18ccd7b27dfca783edb8b3ed5b068524f508d716
Yashwant Kumar Singh <singhyashwant439@gmail.com>
2025-06-28 16:09:47 +0530

    Update passport-setup.js

--- Airbnb/passport-setup.js
+++ Airbnb/passport-setup.js
@@ -11,7 +11,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ googleId: profile.id });
